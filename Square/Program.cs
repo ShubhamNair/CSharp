b1de3f5895a521a7e1f4e0071a450e8d417b69e7
Shubham <88407407+ShubhamNair@users.noreply.github.com>
2022-06-13 04:04:00 +0530

    Add files via upload

---
+++ Square/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Square
+{
+    internal class Area
+    {
+        static void Main(string[] args)
+        {
+            int Side, Area;
+            Console.WriteLine("Enter the length of side:");
+            Side = Convert.ToInt32(Console.ReadLine());
+            Area = Side * Side;
+            Console.WriteLine("Area of Square: {0}", Area);
+            Console.ReadKey();
+        }
+    }
+}
