f0c55ead1f7d32fce0e57b20762e5fdcb98eb68b
Shubham <88407407+ShubhamNair@users.noreply.github.com>
2022-06-13 03:55:53 +0530

    Add files via upload

---
+++ ConvertCase/ConvertCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertCase
+{
+    internal class Convert
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i]++;
+                if (chars[i] >= 65 && chars[i] <= 90)
+                {
+                    chars[i] = Char.ToLowerInvariant(chars[i]);
+                }
+                else if (chars[i] >= 97 && chars[i] <= 122)
+                {
+                    chars[i] = Char.ToUpperInvariant(chars[i]);
+                }
+            }
+            string output = new string(chars);
+
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
