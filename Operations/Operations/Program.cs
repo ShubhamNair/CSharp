a375888e8cd32baa0b8f02dd0c094cf3814b939b
Shubham <88407407+ShubhamNair@users.noreply.github.com>
2022-06-13 04:05:39 +0530

    Add files via upload

---
+++ Operations/Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operations
+{
+    internal class Operators
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1, num2, temp;
+            num2 = ++num1;                  //pre-inceementing num1
+            Console.WriteLine("Pre-incrementing: {0}", num2);
+            num2 = num1++;                  //post-inscreementing num1
+            Console.WriteLine("Post-incrementing: {0}", num2);
+            //Swapping both values
+            Console.WriteLine($"Before Swapping: {num1} and {num2}");
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine($"After swapping: {num1} and {num2}");
+
+        }
+    }
+}
