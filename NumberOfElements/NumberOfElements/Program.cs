3ac78603f2deaaf81e1819dd447ca5c6fde71e6c
Shubham <88407407+ShubhamNair@users.noreply.github.com>
2022-06-13 04:05:19 +0530

    Add files via upload

---
+++ NumberOfElements/NumberOfElements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberOfElements
+{
+    internal class Elements
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
