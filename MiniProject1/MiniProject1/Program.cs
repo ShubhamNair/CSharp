5d1291e2f692cd9e3783501a417fa28ebe4850bb
Shubham <88407407+ShubhamNair@users.noreply.github.com>
2022-06-13 04:08:25 +0530

    Add files via upload

---
+++ MiniProject1/MiniProject1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class UserMainCode
+{
+    public int[] primeEncoder(int[] input1, int input2)
+    {
+        int primeCount = 0;
+        for (int i = 2; i <= 10000; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if (i != j && i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                primeCount++;
+            }
+            if (primeCount == input2)
+            {
+                for (int item = 0; item < input1.Length; item++)
+                {
+                    input1[item] += i;
+                }
+                return input1;
+            }
+        }
+        throw new NotImplementedException("Method primeEncoder(int[] input1,int input2) not Implemented.");
+    }
+}
